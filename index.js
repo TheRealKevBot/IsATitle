42ca070cc02016592b43057df80daf4c1eeef7a9
TheRealKevBot <Kglas001@gmail.com>
2021-07-03 21:20:39 -0600

    First

---
+++ index.js
@@ -0,0 +1,10 @@
+// Check if a string title is a title string or not. 
+// A title string is one which has all the words in the string start with a upper case letter.
+
+function checkTitle(title) {
+	
+}
+
+console.log(checkTitle("A Mind Boggling Achievement"), true)
+console.log(checkTitle("A Simple Java Script Program!"), true)
+console.log(checkTitle("Water is transparent"), false)
